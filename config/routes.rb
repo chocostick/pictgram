a64dae2ff4a85e2a942148813c75a442bc96f4b8
Airi <lv.choco.724@gmail.com>
2021-09-27 01:55:25 +0900

    link集

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'pages/index' 
   root 'pages#index'
   get 'pages/help'
-  
+  get 'pages/link'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
