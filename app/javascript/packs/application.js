843bab2ac13bd3449e8312287b0aad837a21b642
Airi <lv.choco.724@gmail.com>
2021-10-06 20:18:24 +0900

    users/new create

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,4 @@ import "bootstrap"
 import "@fortawesome/fontawesome-free/js/all";
 import "packs/application.scss";
 import "packs/stylesheets/custom.scss"
+import "packs/stylesheets/top.scss"
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get 'pages/help'
   get 'pages/link'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  
 end
