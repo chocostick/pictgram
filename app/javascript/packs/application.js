c1108c2f437f65cc8b35d0e0ed1103c9477765fd
Airi <lv.choco.724@gmail.com>
2021-10-09 20:38:51 +0900

    rails06

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,6 @@ import "@fortawesome/fontawesome-free/js/all";
 import "packs/application.scss";
 import "packs/stylesheets/custom.scss"
 import "packs/stylesheets/top.scss"
+
+var jQuery = require('jquery')
+global.$ = global.jQuery = jQuery;
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'pages/index' 
   root 'pages#index'
   get 'pages/help'
   get 'pages/link'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  get '/logout',  to: 'sessions#destroy'
+  delete '/logout',  to: 'sessions#destroy'
   
 end
