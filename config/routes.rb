4e564d914ba78c9ac428001ef1cb60cf1cbc71d6
Airi <lv.choco.724@gmail.com>
2021-10-15 04:08:07 +0900

    rails08途中

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,17 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/help'
   get 'pages/link'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :topics
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   get '/logout',  to: 'sessions#destroy'
   delete '/logout',  to: 'sessions#destroy'
-  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :topics
+
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+
+
 end
