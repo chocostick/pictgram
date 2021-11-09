0bf5f1eeeaad8bea7a274173fc2cffcdcdf28e5a
Airi <lv.choco.724@gmail.com>
2021-11-09 22:31:19 +0900

    EX07

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,12 +1,13 @@
 class TopicsController < ApplicationController
+before_action :topic_index, only: [:index]
+before_action :set_topics, only: [:new]
+
   def new
     @topic = Topic.new
   end  
 
 
   def index
-    @topics = Topic.all.includes(:favorite_users)
-   
   end
   
   
@@ -25,6 +26,16 @@ class TopicsController < ApplicationController
 
  
   private
+  def topic_index
+    @topics = Topic.all.includes(:favorite_users)
+  end
+
+  def set_topics
+    if @current_user == nil
+      redirect_to("/login")
+    end
+  end
+
   def topic_params
     params.require(:topic).permit(:image, :description)
   end
